5656843cd238d78e8a9b63bebb28da988f009123
jsa0224 <voidness45@gmail.com>
2023-08-30 15:26:03 +0900

    feat: AppAction 객체 생성

---
+++ JuiceMaker/JuiceMaker/Action/AppAction.swift
@@ -0,0 +1,16 @@
+//
+//  AppAction.swift
+//  JuiceMaker
+//
+//  Created by 정선아 on 2023/08/30.
+//
+
+import Foundation
+
+enum AppAction: Equatable {
+    case makingButtonTapped
+    case decrementButtonTapped
+    case incrementButtonTapped
+    case confirmButtonTapped
+    case cancelButtonTapped
+}
