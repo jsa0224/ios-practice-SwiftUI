bc2bb100199998f9b23233924c553f77f1ae61f8
jsa0224 <voidness45@gmail.com>
2023-08-30 15:25:13 +0900

    feat: 프로젝트 파일 생성

---
+++ JuiceMaker/JuiceMaker/JuiceMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceMakerApp.swift
+//  JuiceMaker
+//
+//  Created by 정선아 on 2023/08/30.
+//
+
+import SwiftUI
+
+@main
+struct JuiceMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
